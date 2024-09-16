8ea2ccf942e46170219b032415a25696d9b4cba5
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2024-09-16 13:23:16 +0000

    MISC: Delete separate plotting file.Plot at bottom of runscript

--- convergence_analysis/runscript_space_time_convergence_dirichlet_boundaries.py
+++ convergence_analysis/runscript_space_time_convergence_dirichlet_boundaries.py
@@ -6,13 +6,20 @@ os.environ["NUMEXPR_NUM_THREADS"] = N_THREADS
 os.environ["OMP_NUM_THREADS"] = N_THREADS
 os.environ["OPENBLAS_NUM_THREADS"] = N_THREADS
 
+import sys
 from copy import deepcopy
 
+import matplotlib.pyplot as plt
+import numpy as np
 import porepy as pp
-from porepy.applications.convergence_analysis import ConvergenceAnalysis
-
 from convergence_analysis_models.manufactured_solution_dynamic_3D import ManuMechSetup3d
+from porepy.applications.convergence_analysis import ConvergenceAnalysis
 from utils_convergence_analysis import export_errors_to_txt, run_analysis
+
+sys.path.append("../")
+
+from plotting.plot_utils import (draw_multiple_loglog_slopes,
+                                 fetch_numbers_from_file)
 
 time_steps = 150
 tf = 1.0
@@ -58,3 +65,55 @@ export_errors_to_txt(
     list_of_results=results,
     file_name="displacement_and_traction_errors_space_time.txt",
 )
+
+# Plotting from here and down
+
+values = fetch_numbers_from_file("displacement_and_traction_errors_space_time.txt")
+num_cells = np.array(values["num_cells"]) ** (1 / 3)
+y_disp = values["error_displacement"]
+y_trac = values["error_force"]
+
+# Plot the sample data
+fig, ax = plt.subplots()
+ax.loglog(
+    num_cells,
+    y_disp,
+    "o--",
+    color="firebrick",
+    label="Displacement",
+)
+ax.loglog(
+    num_cells,
+    y_trac,
+    "o--",
+    color="royalblue",
+    label="Traction",
+)
+ax.set_title("Convergence analysis: Spatial and temporal")
+ax.set_xlabel("$(Number\ of\ cells)^{1/3}$")
+ax.set_ylabel("Relative $L^2$ error")
+ax.legend()
+
+# Draw the convergence triangle with multiple slopes
+draw_multiple_loglog_slopes(
+    fig,
+    ax,
+    origin=(0.915 * num_cells[-1], 1.05 * y_disp[-1]),
+    triangle_width=1.0,
+    slopes=[-2],
+    inverted=True,
+    labelcolor=(0.33, 0.33, 0.33),
+)
+
+draw_multiple_loglog_slopes(
+    fig,
+    ax,
+    origin=(1.1 * num_cells[-2], y_trac[-2]),
+    triangle_width=1.0,
+    slopes=[-1.5],
+    inverted=False,
+    labelcolor=(0.33, 0.33, 0.33),
+)
+
+ax.grid(True, which="both", color=(0.87, 0.87, 0.87))
+plt.show()
